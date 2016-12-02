1cc5e1e1e9518383eafc9c3c739b4f269baddfe8
19hour <gnehsaijuhz@hotmail.com>
2016-12-01 20:13:06 -0800

    update

--- CSE167_Final/CSE167_Final/Water.cpp
+++ CSE167_Final/CSE167_Final/Water.cpp
@@ -78,10 +78,9 @@ GLuint useTexture(GLchar* path)
     glGenerateMipmap(GL_TEXTURE_2D);
     
     // Parameters
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
     glBindTexture(GL_TEXTURE_2D, 0);
     return textureID;
 }
